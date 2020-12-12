cc534efd8be8ab7d09411c1978f80f5657d39003
Aaron Dishno Ed.D <adishno@walktheweb.com>
2020-12-11 18:45:57 -0800

    Updated 3.4.2

--- content/plugins/wtw-swiftmailer/scripts/class_main.js
+++ content/plugins/wtw-swiftmailer/scripts/class_main.js
@@ -16,26 +16,26 @@ let wtwswiftmailer = new WTW_SwiftMailer();
 /* activate it with the core WTW.onclick function hook */
 /* in the plugin/functions/class_plugin.php initHooks and initAdminOnlyHooks functions. */
 
-WTW_SwiftMailer.prototype.onClick = function(pickedname) {
+WTW_SwiftMailer.prototype.onClick = function(zpickedname) {
 	try {
-		pickedname = pickedname.toLowerCase();
-		let moldnameparts = WTW.getMoldnameParts(pickedname);
-		/* using moldnameparts, you can get the following values:
-          moldnameparts.moldname is the moldname (pickedname)
-          moldnameparts.moldind is the index number for the mold array
-          moldnameparts.moldid is the unique ID of the mold (database table key value reference)
-          moldnameparts.cgind is the connecting grid index number (WTW.connectingGrids[moldnameparts.cgind] gives you the 3D Object definition)
-          moldnameparts.cgid is the unique ID of the Connecting Grid (database table key value reference)
-          moldnameparts.communityid is the unique ID for the 3D Community related to this 3D Object (only has a value when is a WTW.communityMolds).
-          moldnameparts.buildingid is the unique ID for the 3D Building related to this 3D Object (only has a value when is a WTW.buildingMolds).
-          moldnameparts.thingid is the unique ID for the 3D Thing related to this 3D Object (only has a value when is a WTW.thingMolds).
-          moldnameparts.moldgroup identifies what kind of 3D object it is; building, community, or thing.
-          moldnameparts.molds is the Array for the Mold; WTW.communityMolds, WTW.buildingMolds, or WTW.thingMolds.
-          moldnameparts.shape is the Mold shape which identifies the function used to create the Mold (mesh).
-          moldnameparts.namepart is an array of the segments of the name split by the hyphen '-'. This is useful for checking the additional optional values and current state.
-          moldnameparts.parentname is the full name of the parent 3D Object.
+		zpickedname = zpickedname.toLowerCase();
+		let zmoldnameparts = WTW.getMoldnameParts(zpickedname);
+		/* using zmoldnameparts, you can get the following values:
+          zmoldnameparts.moldname is the moldname (zpickedname)
+          zmoldnameparts.moldind is the index number for the mold array
+          zmoldnameparts.moldid is the unique ID of the mold (database table key value reference)
+          zmoldnameparts.cgind is the connecting grid index number (WTW.connectingGrids[zmoldnameparts.cgind] gives you the 3D Object definition)
+          zmoldnameparts.cgid is the unique ID of the Connecting Grid (database table key value reference)
+          zmoldnameparts.communityid is the unique ID for the 3D Community related to this 3D Object (only has a value when is a WTW.communityMolds).
+          zmoldnameparts.buildingid is the unique ID for the 3D Building related to this 3D Object (only has a value when is a WTW.buildingMolds).
+          zmoldnameparts.thingid is the unique ID for the 3D Thing related to this 3D Object (only has a value when is a WTW.thingMolds).
+          zmoldnameparts.moldgroup identifies what kind of 3D object it is; building, community, or thing.
+          zmoldnameparts.molds is the Array for the Mold; WTW.communityMolds, WTW.buildingMolds, or WTW.thingMolds.
+          zmoldnameparts.shape is the Mold shape which identifies the function used to create the Mold (mesh).
+          zmoldnameparts.namepart is an array of the segments of the name split by the hyphen '-'. This is useful for checking the additional optional values and current state.
+          zmoldnameparts.parentname is the full name of the parent 3D Object.
 
-		  moldnameparts.molds[moldnameparts.moldind] provides the whole 3D Object definition
+		  zmoldnameparts.molds[zmoldnameparts.moldind] provides the whole 3D Object definition
 		  see /core/scripts/prime/wtw_objectdefinitions.js for full object references
 		*/
 	} catch (ex) {
