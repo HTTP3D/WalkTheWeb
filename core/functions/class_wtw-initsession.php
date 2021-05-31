fa3db4ed9c7b776115d66b00744bea215496adf2
Aaron Dishno Ed.D <adishno@walktheweb.com>
2021-05-31 12:00:36 -0700

    Updated 3.4.7

--- core/functions/class_wtw-initsession.php
+++ core/functions/class_wtw-initsession.php
@@ -21,9 +21,9 @@ class wtw {
 	}	
 	
 	/* declare public $wtw variables */
-	public $version = "3.4.6";
+	public $version = "3.4.7";
 	public $dbversion = "1.1.18";
-	public $versiondate = "2021-4-30";
+	public $versiondate = "2021-5-31";
 	public $serverinstanceid = "";
 	public $usertoken = "";
 	public $rootpath = "";
