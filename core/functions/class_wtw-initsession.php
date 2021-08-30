1e1153933a508624640cd2aa08358d7e1355f8d7
Aaron Dishno Ed.D <adishno@walktheweb.com>
2021-08-30 12:42:26 -0700

    Updated 3.4.9
    incremented the Version and release date.

--- core/functions/class_wtw-initsession.php
+++ core/functions/class_wtw-initsession.php
@@ -21,9 +21,9 @@ class wtw {
 	}	
 	
 	/* declare public $wtw variables */
-	public $version = "3.4.8";
+	public $version = "3.4.9";
 	public $dbversion = "1.1.18";
-	public $versiondate = "2021-6-30";
+	public $versiondate = "2021-8-30";
 	public $serverinstanceid = "";
 	public $usertoken = "";
 	public $rootpath = "";
