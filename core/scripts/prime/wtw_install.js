b40c921b7a3c837b9923249543be46dc8af6a0db
Aaron Dishno Ed.D <adishno@walktheweb.com>
2022-03-08 14:09:45 -0800

    Updated 3.4.12
    Rebranded the Styled the Install Process - We rebranded and styled the Install Process so that it uses the same logo that displays on the bottom left, black background based, and larger fonts. We also moved inline styles to the stylesheet. 
    
    New Install Step - Optional Services (Multiplayer) - We now added an additional screen to the setup process to offer Optional Services. The first service offered is to enable multiplayer. There are 3 basic multiplayer plans to start; Developer, Small Business, and Gamers. You can either purchase a year of service or simply click No Thanks. After the step is shown once, it will not show again. The same settings and ability to turn on Multiplayer is available in the Admin Menu, 3D Internet, Control Panel.

--- core/scripts/prime/wtw_install.js
+++ core/scripts/prime/wtw_install.js
@@ -229,6 +229,20 @@ WTWJS.prototype.hide = function(zelement) {
 	}
 }
 
+WTWJS.prototype.toggle = function(item) {
+	try {
+		if (dGet(item) != null) {
+			if (dGet(item).style.display == 'none') {
+				WTW.show(item);
+			} else {
+				WTW.hide(item);
+			}
+		}
+	} catch (ex) { 
+		WTW.log("core-scripts-prime-wtw_login.js-toggle=" + ex.message);
+	}
+}
+
 WTWJS.prototype.cleanInvalidCharacters = function(zvalue) {
 	/* remove line breaks and other select non text characters from string */
 	try {
@@ -285,3 +299,256 @@ WTWJS.prototype.isNumeric = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+WTWJS.prototype.selectMultiplayerPackage = function(zobj) {
+	try {
+		dGet('wtw_business').className = 'wtw-servicelisting';
+		dGet('wtw_gamer').className = 'wtw-servicelisting';
+		dGet('wtw_developer').className = 'wtw-servicelisting';
+		if (zobj != null) {
+			zobj.className = 'wtw-servicelisting-selected';
+			switch (zobj.id) {
+				case 'wtw_business':
+					dGet('wtw_selectedservice').innerHTML = 'Multiplayer for Small Businesses';
+					dGet('wtw_selectedprice').innerHTML = '$20';
+					break;
+				case 'wtw_gamer':
+					dGet('wtw_selectedservice').innerHTML = 'Multiplayer for 3D Game Websites';
+					dGet('wtw_selectedprice').innerHTML = '$27';
+					break;
+				case 'wtw_developer':
+					dGet('wtw_selectedservice').innerHTML = 'Multiplayer for Developers';
+					dGet('wtw_selectedprice').innerHTML = '$10';
+					break;
+			}
+		}
+	} catch (ex) { 
+		WTW.log("core-scripts-prime-wtw_install.js-selectMultiplayerPackage=" + ex.message);
+	}
+}
+
+WTWJS.prototype.selectExpandedPackage = function(zobj) {
+	try {
+		if (zobj != null) {
+			if (zobj.className == 'wtw-servicelisting') {
+				zobj.className = 'wtw-servicelisting-selectedtoggle';
+				WTW.show('wtw_expandedservice');
+			} else {
+				zobj.className = 'wtw-servicelisting';
+				WTW.hide('wtw_expandedservice');
+			}
+		}
+	} catch (ex) { 
+		WTW.log("core-scripts-prime-wtw_install.js-selectExpandedPackage=" + ex.message);
+	}
+}
+
+WTWJS.prototype.openCart = function() {
+	try {
+		var zselected = 'business';
+		
+		if (dGet('wtw_business') != null) {
+			if (dGet('wtw_business').className == 'wtw-servicelisting-selected') {
+				zselected = 'business';
+			} else if (dGet('wtw_gamer').className == 'wtw-servicelisting-selected') {
+				zselected = 'gamer';
+			} else if (dGet('wtw_developer').className == 'wtw-servicelisting-selected') {
+				zselected = 'developer';
+			}
+		}
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+		WTW.openIFrame("https://3dnet.walktheweb.com/core/pages/cartwalktheweb.php?serverinstanceid=" + btoa(dGet('wtw_serverinstanceid').value) + "&serverip=" + btoa(dGet('wtw_serverip').value) + "&domainname=" + btoa(dGet('wtw_domainname').value) + "&domainurl=" + btoa(dGet('wtw_domainurl').value) + "&websiteurl=" + btoa(dGet('wtw_websiteurl').value) + "&userid=" + btoa(dGet('wtw_userid').value) + "&useremail=" + btoa(dGet('wtw_useremail').value) + "&usertoken=" + btoa(dGet('wtw_usertoken').value) + "&selected=" + btoa(zselected), .8, .8, "WalkTheWeb Shopping Cart");
+
+/*		var zrequest = {
+			'serverinstanceid':btoa(dGet('wtw_serverinstanceid').value),
+			'serverip':btoa(dGet('wtw_serverip').value),
+			'domainurl':btoa(dGet('wtw_domainurl').value),
+			'websiteurl':btoa(dGet('wtw_websiteurl').value),
+			'domainname':btoa(dGet('wtw_domainname').value),
+			'userid':btoa(dGet('wtw_userid').value),
+			'useremail':btoa(dGet('wtw_useremail').value),
+			'usertoken':btoa(dGet('wtw_usertoken').value),
+			'function':'opencart'
+		};
+		WTW.postJSON("https://3dnet.walktheweb.com/core/pages/cartwalktheweb.php", zrequest, 
+			function(zresponse) {
+				zresponse = JSON.parse(zresponse);
+				
+			}
+		);
+*/		
+	} catch (ex) { 
+		WTW.log("core-scripts-prime-wtw_install.js-openCart=" + ex.message);
+	}
+}
+
+WTWJS.prototype.openIFrame = function(zurl, zwidth, zheight, ztitle) {
+	/* open iframe page with frame window (includes title and close x), height and width (values should be between .1 and 1) */
+	try {
+		if (ztitle == undefined) {
+			ztitle = "";
+		}
+		WTW.setWindowSize();
+		if (typeof zwidth === "undefined" || zwidth === null) {
+			zwidth = .9; 
+		}
+		if (typeof zheight === "undefined" || zheight === null) {
+			zheight = .9; 
+		}
+		WTW.hide('wtw_ipagediv');
+		WTW.show('wtw_ibrowseframe');
+		var ziframe = dGet('wtw_ibrowseframe');
+		if (ziframe.src != zurl) {
+			ziframe.src = zurl;
+		}
+		dGet('wtw_ibrowsediv').style.width = Math.round(WTW.sizeX * zwidth) + "px";
+		dGet('wtw_ibrowsediv').style.height = Math.round(WTW.sizeY * zheight) + "px";
+		dGet('wtw_ibrowsediv').style.left = Math.round((WTW.sizeX * (1 - zwidth)) / 2) + "px";
+		dGet('wtw_ibrowsediv').style.top = Math.round((WTW.sizeY * (1 - zheight)) / 2) + "px";
+		dGet('wtw_ibrowsediv').style.display = "inline-block";
+		dGet('wtw_ibrowsediv').style.visibility = "visible";
+		dGet('wtw_ibrowsediv').style.zIndex = 3000;
+		dGet('wtw_ibrowsediv').style.backgroundColor = '#ffffff';
+		if (zurl == '/core/pages/help.php') {
+			ziframe.onload = function() { WTW.setHelp();	};
+			dGet('wtw_browsetitle').innerHTML = "WalkTheWeb - Help";
+		} else {
+			dGet('wtw_browsetitle').innerHTML = ztitle;
+		}
+	} catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-openIFrame=" + ex.message);
+		WTW.closeIFrame();
+//		WTW.openWebpage(zurl, '_blank');
+	}
+}
+
+WTWJS.prototype.cartComplete = function() {
+	/* closes shopping cart and the iframe window frame */
+	try {
+		/* close iframe */
+		WTW.closeIFrame();
+		/* refresh page to continue install */
+		window.location.href=window.location.href;
+	} catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-cartComplete=" + ex.message);
+	}
+}
+	
+WTWJS.prototype.closeIFrame = function() {
+	/* closes the iframe window frame */
+	try {
+		var ziframe = dGet('wtw_ibrowseframe');
+		ziframe.onload = function() {};
+		dGet('wtw_ibrowsediv').style.zIndex = 0;
+		dGet('wtw_ibrowsediv').style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+		dGet('wtw_ibrowsediv').style.display = "none";
+		dGet('wtw_ibrowsediv').style.visibility = "hidden";
+		dGet('wtw_browsetitle').innerHTML = "";
+		dGet('wtw_ibrowseframe').src = "/core/pages/loading.php";
+	} catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-closeIFrame=" + ex.message);
+	}
+}
+
+WTWJS.prototype.setWindowSize = function() {
+	/* set window size and reset proportional elements as needed */
+    try {
+        if (typeof (window.innerWidth) == 'number') { /* Non-IE */
+            WTW.sizeX = window.innerWidth;
+            WTW.sizeY = window.innerHeight;
+        } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) { /* IE 6+ */
+            WTW.sizeX = document.documentElement.clientWidth;
+            WTW.sizeY = document.documentElement.clientHeight;
+        } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) { /* older */
+            WTW.sizeX = document.body.clientWidth;
+            WTW.sizeY = document.body.clientHeight;
+        }
+		if (dGet('wtw_ibrowsediv').style.display != "none") {
+			var zwidth = dGet('wtw_ibrowsediv').clientWidth;
+			var zheight = dGet('wtw_ibrowsediv').clientHeight;
+			if (WTW.isNumeric(zwidth)) {
+				dGet('wtw_ibrowsediv').style.width = Math.round(Number(zwidth)) + "px";
+				dGet('wtw_ibrowsediv').style.left = Math.round((WTW.sizeX - Number(zwidth)) / 2) + "px";
+			}
+			if (WTW.isNumeric(zheight)) {
+				dGet('wtw_ibrowsediv').style.height = Math.round(Number(zheight)) + "px";
+				dGet('wtw_ibrowsediv').style.top = Math.round((WTW.sizeY - Number(zheight)) / 2) + "px";
+			}
+			dGet('wtw_ibrowsediv').style.display = "inline-block";
+			dGet('wtw_ibrowsediv').style.visibility = "visible";
+			dGet('wtw_ibrowsediv').style.zIndex = 3000;
+		}
+    } catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-setWindowSize=" + ex.message);
+    }
+}
+
+WTWJS.prototype.onMessage = function (zevent) {
+	/* message listener is enabled and this function can receive predefined messages from an iframe within the WalkTheWeb instance */
+	try {
+		zevent = zevent || window.event;
+		let zsafe = false;
+		// Check sender origin to be trusted
+		if (zevent.origin == "https://3d.walktheweb.com") {
+			zsafe = true;
+		} else if (zevent.origin == "https://3dnet.walktheweb.com") {
+			zsafe = true;
+		} else if (zevent.origin == "https://3dnet.walktheweb.network") {
+			zsafe = true;
+		} else if (zevent.origin == wtw_domainurl) {
+			zsafe = true;
+		}
+		if (zsafe) {
+			let zfunctionname = '';
+			if (zevent.data.func != undefined) {
+				zfunctionname = zevent.data.func;
+			}
+			let zparameters = null;
+			if (zevent.data.parameters != undefined) {
+				zparameters = zevent.data.parameters;
+			}
+			let zmessage = null;
+			if (zevent.data.message != undefined) {
+				zmessage = zevent.data.message;
+			}
+			if (zfunctionname != '') {
+				WTW.executeFunctionByName(zfunctionname, window, zparameters);
+			}
+		}
+	} catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-onMessage=" + ex.message);
+	}
+}
+
+WTWJS.prototype.executeFunctionByName = function(zjsfunction, zcontext /*, args */) {
+	/* allows a name of a javascript function to be passed (even with class like WTW.testScript) with parameters string (zcontext) to be executed */
+	/* example: WTW.executeFunctionByName('WTW.testScript', 'arg1', 'arg2', 'arg3', 'arg4');   executes: WTW.testScript('arg1', 'arg2', 'arg3', 'arg4'); */
+	var zfunc = null;
+	var zargs = null;
+	var zfunction = null;
+	try {
+		if (zjsfunction != null) {
+			if (zjsfunction != '') {
+				zargs = Array.prototype.slice.call(arguments, 2);
+				var znamespaces = zjsfunction.split(".");
+				zfunc = znamespaces.pop();
+				for(var i = 0; i < znamespaces.length; i++) {
+					zcontext = zcontext[znamespaces[i]];
+				}
+				if (typeof zcontext[zfunc] == 'function') {
+					zfunction = zcontext[zfunc].apply(zcontext, zargs);
+				}
+			}
+		}
+	} catch (ex) {
+		WTW.log("core-scripts-prime-wtw_install.js-executeFunctionByName=" + ex.message);
+	}
+	return zfunction;
+}
+
+window.onload = function() {
+	if (window.addEventListener) {
+		window.addEventListener("message", WTW.onMessage, false);        
+	} else if (window.attachEvent) {
+		window.attachEvent("onmessage", WTW.onMessage, false);
+	}
+}
