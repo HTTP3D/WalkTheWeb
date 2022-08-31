692c57c5f1ddd7a9b142be63477f76344258d329
Aaron Dishno Ed.D <adishno@walktheweb.com>
2022-08-31 10:56:02 -0700

    Deleted 3.5.2
    Code moved to adminmenu and menu classes

--- core/forms/groundsettings.php
+++
@@ -1,7 +0,0 @@
-<!-- admin menu form for editing the extended ground texture -->
-<div id="wtw_adminmenu41b" class="wtw-hide">
-	<h2>Ground Texture Image:</h2>
-	<img id="wtw_showextendedgroundpreview" class="wtw-previewimage" /><br /><br />
-	<div class='wtw-menulevel2' onclick="WTW.openFullPageForm('medialibrary','image','extendedgroundtexture','wtw_textendedgroundtextureid','wtw_textendedgroundtexturepath','wtw_showextendedgroundpreview');" style="cursor: pointer;" />Change Texture</div>
-	<br />
-</div>
